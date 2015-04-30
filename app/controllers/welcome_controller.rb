eec205f2870606dad7ea9b34befaa52a4a131319
akarnani1 <abhishek.karnani224@gmail.com>
2015-04-30 05:45:00 +0530

    init

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,11 @@
 class WelcomeController < ApplicationController
+    after_action :allow_iframe, only: :index
+	def index
+	end
+
+    private
+
+	def allow_iframe
+		response.headers['X-Frame-Options'] = "ALLOWALL"
+	end
 end
